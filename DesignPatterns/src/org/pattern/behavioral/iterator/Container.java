a066e0fd5708df82e40b70e044561e401f98106c
mghilardi <mauricio.ghilardi@gmail.com>
2015-04-03 22:26:08 +0100

    Added Iterator Pattern

---
+++ DesignPatterns/src/org/pattern/behavioral/iterator/Container.java
@@ -0,0 +1,5 @@
+package org.pattern.behavioral.iterator;
+
+public interface Container {
+	public Iterator getIterator();
+}
---
+++ DesignPatterns/src/org/pattern/behavioral/iterator/Iterator.java
@@ -0,0 +1,7 @@
+package org.pattern.behavioral.iterator;
+
+public interface Iterator {
+	public boolean hasNext();
+
+	public Object next();
+}
