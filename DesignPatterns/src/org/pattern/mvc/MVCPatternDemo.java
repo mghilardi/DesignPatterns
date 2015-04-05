a7f8cd138f4be33827bb282a89f47db5b263653d
mghilardi <mauricio.ghilardi@gmail.com>
2015-04-05 06:16:19 +0100

    Added MVC Pattern

---
+++ DesignPatterns/src/org/pattern/mvc/MVCPatternDemo.java
@@ -0,0 +1,32 @@
+package org.pattern.mvc;
+
+import org.pattern.mvc.model.Student;
+import org.pattern.mvc.model.view.StudentView;
+import org.pattern.mvc.model.view.controller.StudentController;
+
+public class MVCPatternDemo {
+	public static void main(String[] args) {
+
+		// fetch student record based on his roll no from the database
+		Student model = retriveStudentFromDatabase();
+
+		// Create a view : to write student details on console
+		StudentView view = new StudentView();
+
+		StudentController controller = new StudentController(model, view);
+
+		controller.updateView();
+
+		// update model data
+		controller.setStudentName("John");
+
+		controller.updateView();
+	}
+
+	private static Student retriveStudentFromDatabase() {
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+}
